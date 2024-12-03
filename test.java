75cec4fbff60c96ddd9a87f42e1ce948123e556f
Yope Man <yopeman318@gmail.com>
2024-12-03 19:36:40 +0300

    I remove Run.java and test,java

--- test.java
+++
@@ -1,8 +0,0 @@
-import database.DB;
-
-public class test {
-    public static void main(String[] args) {
-        String sql = "select * from user";
-        DB.display_query(sql, "%-20s", 50);
-    }
-}
